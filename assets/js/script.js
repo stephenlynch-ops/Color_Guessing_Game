89fc5313f9cb4cd6b914c6f88fa463fbd36ea83a
Stephen Lynch <stephen.lynch17@icloud.com>
2022-02-14 20:05:14 +0000

    FEAT: Set up basic version of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Play Game!");
             } else {
                 let playersChoice = this.getAttribute("data-type");
-                alert(`You clicked ${playersChoice}!`);
+                runGame(playersChoice);
             }
         })
     }
 })
 
 
-function runGame() {
+function runGame(playersChoice) {
 
+    var ranNum = Math.floor(Math.random() * 4) + 1;
+    
+    console.log(ranNum);
+
+    if (ranNum === 1) {
+        var randomColor = "red";
+    } else if (ranNum === 2) {
+        var randomColor = "blue";
+    } else if (ranNum === 3) {
+        var randomColor = "green";
+    } else { 
+        var randomColor = "pink";
+    }
+
+    console.log(randomColor)
+
+    if (randomColor === playersChoice) {
+        alert (`You won, both you and computer picked ${playersChoice}`);
+    } else {
+        alert (`You lost, you choice ${playersChoice} does not match the computers choice, which was ${randomColor}`);
+    }
 }
 
 function convertNumberToColor() {
